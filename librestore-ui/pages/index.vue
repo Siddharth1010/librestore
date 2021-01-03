4614471611502bba926912277d9d9d01e0575ba8
Amelia Ruzek <amelia.ruzek.klein@gmail.com>
2021-01-03 08:51:32 -0600

    add missing files

---
+++ librestore-ui/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Product title="Carrots" price="3.99"></Product>
+    <Product title="Beans" price="1.50"></Product>
+    <Product title="Bacon" price="9.00"></Product>
+    <Product title="Rice" price="5.99"></Product>
+  </div>
+</template>
+
+<script>
+import Product from '~/components/Product.vue'
+
+export default {
+  components: {
+    Product,
+  },
+}
+</script>
+<style scoped>
+div {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+</style>
